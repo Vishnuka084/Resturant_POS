b0c44019aa68959bdad7a2f5535a0078e175cc6a
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-28 17:41:20 +0530

    updated Contet

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import {Link} from "react-router-dom";
-
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function MainLayout({children}){
     return (
         <div>
