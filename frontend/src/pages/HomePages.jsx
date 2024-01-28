0d1f3e363bbf9af5867c1b2a2614b0fd30f91bae
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-28 17:00:52 +0530

    updated contne

--- frontend/src/pages/HomePages.jsx
+++ frontend/src/pages/HomePages.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
 function HomePages() {
     return(
 
-        <div>HomePages</div>
+        <div>
+            <header>
+                <nav className="navbar navbar-light bg-primary">
+                    <div className="container">
+                        <Link to="/" className="navbar-brand" href="#">DevPos</Link>
+                    </div>
+                </nav>
+            </header>
+            <main>
+
+            </main>
+        </div>
     )
 }
 
