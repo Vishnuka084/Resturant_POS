13c80f11e07f4e21bcfa09624a6e02f97937fc3b
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-28 17:06:06 +0530

    updated contne

--- frontend/src/pages/HomePages.jsx
+++ frontend/src/pages/HomePages.jsx
@@ -12,6 +12,18 @@ function HomePages() {
                 </nav>
             </header>
             <main>
+                <div className='container mt-3'>
+                    <div className='bg-light p-5 mt-4 rounded-3'>
+                        <h1>Welcome to the simple Pos small business </h1>
+                        <p>Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit.
+                            Aperiam, eligendi molestiae nisi
+                            numquam placeat possimus quas reprehenderit
+                            repudiandae sunt ullam? od reiciendis! </p>
+                        <p>If you have an issue call 0764615235</p>
+                        <Link to="/pos" className='btn btn-primary'>Click here to sell products</Link>
+                    </div>
+                </div>
 
             </main>
         </div>
